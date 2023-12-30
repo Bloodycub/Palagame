df9782b2ffc20642b0a1c7ec18645e25598b6f6e
Ivi <Hiltunen.ivi@gmail.com>
2023-12-30 21:00:48 +0200

    v3

---
+++ jotain.py
@@ -0,0 +1,156 @@
+import time
+import cProfile
+from itertools import groupby
+
+def getpoints(array):
+    def check_adjacent(lst):
+        lengths = []
+        count = 1
+        prev_element = lst[0]
+
+        for current_element in lst[1:]:
+            if current_element == prev_element:
+                count += 1
+            else:
+                if count >= 3:
+                    lengths.append(count)
+                count = 1
+
+            prev_element = current_element
+
+        if count >= 3:
+            lengths.append(count)
+
+        return lengths
+
+
+
+
+    def transpose(matrix):
+        return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+    def calculatepoints(starlevel):
+        '''
+        Scoring is based on https://yppedia.puzzlepirates.com/Bilge_scoring
+        Length 3 = 3 points
+        Length 4 = 5 points
+        Length 5 = 7 points
+        NxN (double): 2 times
+        NxNxN (bingo): 3 times
+        Sea Donkey: At least 4 times, possibly 5 times.
+        Vegas: At least 5 times, possibly 6 or 7 times.
+        '''
+        # TODO: scale points calculation based on star level
+        # Lower star level means less points per combo
+
+        lines = rows_lengths + columns_lengths
+        basepoints = 0
+        multiplier = len(lines)
+
+        if multiplier == 4:
+            multiplier = 4.5
+            if 5 in lines:
+                multiplier = 6
+
+        for line in lines:
+            if line == 3:
+                basepoints += 3
+            elif line == 4:
+                basepoints += 5
+            elif line == 5:
+                basepoints += 7
+
+        points = basepoints*multiplier
+        return points
+
+    rows_lengths = [length for row in array for length in check_adjacent(row)]
+    columns_lengths = [length for column in transpose(array) for length in check_adjacent(column)]
+
+    return calculatepoints(0)
+
+def comparemoves(move1, move2):
+    m1_score = move1[0]
+    m2_score = move2[0]
+    m1 = move1[0]/( len(move1) - 1 )
+    m2 = move2[0]/( len(move2) - 1 )
+    if m1_score == m2_score:
+        return move1 if len(move1) < len(move2) else move2
+    return move1 if m1_score > m2_score else move2
+
+def checkpuffer(array):
+    for y in range(12):
+        for x in range(6):
+            if array[y][x] == 69:
+                return (x,y)
+    return (-1,-1)
+
+def getbestmove(array, iterations):
+    # if a move set above this score is found, immediately stop calculating
+    # treshold of 45 means AT LEAST 15 points per move
+    treshold_score = 666
+
+    def swap(listxy):
+        x, y = listxy
+        copy_of_array = [row[:] for row in array]
+        copy_of_array[y][x], copy_of_array[y][x+1] = copy_of_array[y][x+1], copy_of_array[y][x]
+        return copy_of_array
+
+
+    moves: list[int|list[int]] = []
+
+    for y in range(len(array)):
+        for x in range(len(array[y]) - 1):
+            result_array = swap([x, y])
+            points = getpoints(result_array)
+            move = [points, [x,y]]
+            moves.append(move)
+
+    if iterations > 0:
+        for i in range(len(moves)-1, 0, -1):
+            if moves[i][0] == 0:
+                array_new = swap(moves[i][1])
+                best_new = getbestmove(array_new, iterations-1)
+                moves[i][0] = best_new[0]
+                for x in range(1,len(best_new)):
+                    moves[i].append(best_new[x])
+                if best_new[0] >= treshold_score:
+                    return moves[i]
+
+    best = [0,[0, 0]]
+    for move in moves:
+        best = comparemoves(best, move)
+
+    return best
+
+
+example_array = [
+  [3, 4, 5, 2, 1, 3],
+  [2, 1, 3, 5, 4, 1],
+  [4, 3, 4, 3, 2, 4],
+  [1, 5, 4, 2, 5, 2],
+  [4, 4, 2, 4, 2, 2],
+  [3, 5, 4, 1, 2, 3],
+  [1, 4, 3, 2, 5, 2],
+  [4, 5, 1, 1, 3, 5],
+  [2, 3, 4, 5, 1, 3],
+  [5, 1, 2, 3, 4, 1],
+  [2, 3, 1, 5, 4, 3],
+  [1, 5, 4, 3, 2, 1]
+]
+
+
+
+
+if __name__ == "__main__":
+    cProfile.run('getbestmove(example_array, 2)')
+    start_time = time.time()
+
+    bestmove = getbestmove(example_array, 2)
+    print("Best move gives score:",bestmove[0])
+    print("Swaps to make:")
+    for i in range(1,len(bestmove)):
+        print(f"{bestmove[i]}")
+
+    end_time = time.time()
+    runtime = end_time-start_time
+    print(f"Runtime is {runtime:.4f} seconds")
--- name.py
+++ name.py
@@ -1,6 +1,9 @@
 import pygetwindow as gw
 import pyautogui
 import time
+import jotain
+import keyboard
+import sys
 
 RESET = '\033[0m'
 BLUE = '\033[34m'
@@ -10,130 +13,151 @@ GREEN = '\033[32m'
 CYAN = '\033[36m'
 BRIGHT_GREEN = '\033[92m'
 BRIGHT_WHITE = '\033[97m'
+BRIGHT_YELLOW = '\033[93m'
+BG_MAGENTA = '\033[45m'
 
-gamestate: list[list[int]] = []
+STOP_FLAG = False
 
+gamestate : list[list[int]] = []
 
 def print_colored_text(text, color_code):
+    return
     print(f"{color_code}{text}{RESET}", end=" ")
 
-
 def create_empty_array(xsize: int, ysize: int) -> list[list[int]]:
-    # Create a 2D array with zeros using lists
-    return [[0 for _ in range(xsize)] for _ in range(ysize)]
 
+    # Create a 2D array with zeros using lists
+    two_d_array = [[0 for _ in range(xsize)] for _ in range(ysize)]
+
+    return two_d_array
 
 def get_gameboard_top_left(window):
     left, top = window.left, window.top
     offset_x = 90 + 3
     offset_y = 77
-    return left + offset_x, top + offset_y
+    return left+offset_x, top+offset_y
 
-
-def get_piece_coordinate(window, right_amount_blocks, down_amount_blocks):
-    # Start From First Object
+def get_piece_coordinate(window, right_amount_blocks,down_amount_blocks): # Start From First Object
     left, top = window.left, window.top
-    offset_x = 90 + 3 + right_amount_blocks * 45 - 22
-    offset_y = 77 + down_amount_blocks * 45 - 22
+    offset_x = 90 + 3 + right_amount_blocks*45 - 22
+    offset_y = 77 + down_amount_blocks*45 - 22
 
-    return left + offset_x, top + offset_y
+    xcoord = left+offset_x
+    ycoord = top+offset_y
+
+    return xcoord, ycoord
 
 
+def check_color(pixel_color, piece_color):
+    treshold = 2
+    for i in range(3):
+        if pixel_color[i] not in range(piece_color[i]-treshold, piece_color[i]+treshold):
+            return False
+    return True
+
+
+
+#def get_color_of_object(sideways, vertical):
 def get_pixel_color(x_coordinate, y_coordinate):
     pixel_color = pyautogui.pixel(x_coordinate, y_coordinate)
-    color_mapping = {
-        (26, 199, 242): CYAN,
-        (11, 125, 193): CYAN,
-        (34, 102, 210): BLUE,
-        (14, 86, 181): BLUE,
-        (152, 216, 251): BRIGHT_WHITE,
-        (62, 132, 197): BRIGHT_WHITE,
-        (76, 149, 140): GREEN,
-        (31, 105, 153): GREEN,
-        (122, 219, 192): BRIGHT_GREEN,
-        (50, 133, 174): BRIGHT_GREEN
-    }
+    if(check_color(pixel_color, (26, 199, 242)) or check_color(pixel_color,(11, 125, 193))): # blue circle
+        print_colored_text("¤",CYAN)
+        return 1
+    elif(check_color(pixel_color, (34, 102, 210)) or check_color(pixel_color,(14, 86, 181))): # blue square
+        print_colored_text("¤",BLUE)
+        return 2
+    elif(check_color(pixel_color,(152, 216, 251)) or check_color(pixel_color,(62, 132, 197))): # blue octagon
+        print_colored_text("¤",BRIGHT_WHITE)
+        return 3
+    elif(check_color(pixel_color,(76, 149, 140)) or check_color(pixel_color,(31, 105, 153))): # green octagon
+        print_colored_text("¤",GREEN)
+        return 4
+    elif(check_color(pixel_color,(122, 219, 192)) or  check_color(pixel_color,(50, 133, 174))): # greencircle
+        print_colored_text("¤",BRIGHT_GREEN)
+        return 5
+    elif(check_color(pixel_color,(21, 191, 199)) or  check_color(pixel_color,(9, 122, 177))): # greencircle
+        print_colored_text("¤",BG_MAGENTA)
+        return 6
+    elif(check_color(pixel_color,(145, 132, 83)) or  check_color(pixel_color,(59, 98, 130))): # puffer Fish
+        print_colored_text("¤",BRIGHT_YELLOW)
+        return 69
+    else:
+        return -1
 
-    for color, code in color_mapping.items():
-        if pixel_color == color:
-            print_colored_text("¤", code)
-            return color_mapping[color]
-
-    return RESET
-
+def get_pixel_color_rgb(x, y):
+    pixel_color = pyautogui.pixel(x, y)
+    print(pixel_color, end=" ")
+    return 0
 
 def get_all_pixel_colors(window):
     for y in range(12):
         for x in range(6):
-            xcoord, ycoord = get_piece_coordinate(window, x + 1, y + 1)
-            piece = get_pixel_color(xcoord, ycoord)
-        print()
+            xcoord, ycoord = get_piece_coordinate(window, x+1, y+1)
+            piece = get_pixel_color(xcoord, ycoord) # get_pixel_color_rgb Print For Values
+            # _ = get_pixel_color_rgb(xcoord, ycoord) # get_pixel_color_rgb Print For Values
+            gamestate[y][x] = piece
+        # print("")
 
-
-def find_best_move():
+def ifpause():
     for y in range(12):
-        for x in range(5):
-            pass
+        for x in range(6):
+            if gamestate[y][x] == -1:
+                return True
+    return False
 
-
-def calculate_score(array):
-    def calculate_group_score(lst):
-        score = 0
-        count = 1
-        for i in range(1, len(lst)):
-            if lst[i] == lst[i - 1]:
-                count += 1
-            else:
-                if count >= 3:
-                    score += count
-                count = 1
-        if count >= 3:
-            score += count
-        return score
-
-    def transpose(matrix):
-        return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-
-    total_score = 0
-
-    # Calculate score for rows
-    for row in array:
-        total_score += calculate_group_score(row)
-
-    # Calculate score for columns
-    for column in transpose(array):
-        total_score += calculate_group_score(column)
-
-    return total_score
+def on_key_event(k):
+    global STOP_FLAG
+    if k.name == "esc":
+        print("Stopping the script...")
+        STOP_FLAG = True
 
 
 if __name__ == "__main__":
+    keyboard.hook(on_key_event)
     full_window_title = "Puzzle Pirates - Yolomaister on the Emerald ocean"  # Replace with your actual game window title
 
     print("Starting bot...")
 
     # Initialize gamestate array with 6x12 matrix of zeros
-    gamestate = create_empty_array(6, 12)
+    gamestate = create_empty_array(6,12)
 
     # Get Game Window
     full_game_window = gw.getWindowsWithTitle(full_window_title)
     window = full_game_window[0]
-    window.activate()  # Opens windows and uses.
 
     if window:
+        window.activate() # Opens windows and uses.
+
         # get piece coordinates and store them in x, y variables
         x, y = get_piece_coordinate(window, 1, 1)
 
         # move mouse to coordinates with 0.5 second delay
-        pyautogui.moveTo(x, y, duration=0.5)
         # left click
-        pyautogui.click()
-        time.sleep(1)
+       # pyautogui.click()
+        while not STOP_FLAG:
+            get_all_pixel_colors(window)
+            if ifpause():
+                pyautogui.sleep(2)
+                continue
+            pufferxy = jotain.checkpuffer(gamestate)
+            if pufferxy != (-1,-1):
+                x, y = get_piece_coordinate(window, pufferxy[0]+1,pufferxy[1]+1)
+                pyautogui.moveTo(x, y, duration=0.5)
+                pyautogui.click()
+                pyautogui.sleep(2)
+                continue
+            bestmove = jotain.getbestmove(gamestate, 2)
+            print("Best move gives score:",bestmove[0])
+            print("Swaps to make:")
+            for i in range(1,len(bestmove)):
+                print(f"{bestmove[i][0]+1} {bestmove[i][1]+1}")
+                x, y = get_piece_coordinate(window, bestmove[i][0]+1, bestmove[i][1]+1)
+                x += 10
+                pyautogui.moveTo(x, y, duration=0.2)
+                pyautogui.click()
+                pyautogui.sleep(0.1)
 
-        # if object_found:
-        #     # Interact with the object (e.g., click on it)
-        #     pyautogui.click(100, 200)
-        get_all_pixel_colors(window)
-        print("Bot finished.")
+            pyautogui.sleep(2)
+
     else:
         print(f"Game window with title '{full_window_title}' not found.")
